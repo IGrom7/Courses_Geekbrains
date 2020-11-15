5e4a803bfd7871ea0892662a8089ab6315cf6dd2
IGrom <Gold64rus@gmail.com>
2020-11-15 20:57:12 +0300

    Сделана пятая домашка

--- src/ru/geekbrains/com/Main.java
+++ src/ru/geekbrains/com/Main.java
@@ -1,44 +1,18 @@
 package ru.geekbrains.com;
 
-import java.util.Scanner;
-
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Ваша задача угадать число.");
-        playLevel(9, 3,0);
-        scanner.close();
-    }
-
-    private static int playLevel(int range, int lives, int g) {
-        int number = (int) (Math.random() * range);
-        while (lives > 0) {
-            System.out.println("Колличество попыток " + lives);
-            System.out.println("Угадайте число от 0 до " + range);
-            int intpun_number = scanner.nextInt();
-            if (intpun_number == number) {
-                System.out.println("Вы выиграли!");
-                break;
-            } else if (intpun_number > number) {
-                lives--;
-                System.out.println("Число меньше загаданного...");
-            } else if (intpun_number < number) {
-                lives--;
-                System.out.println("Число больше загаданного...");
-            }
-            if (lives==0) {
-                System.out.println("Вы проиграли =(");
-                System.out.println("Сыграть ещё раз? 1-Да!,2-Нет.");
-                int againGame = scanner.nextInt();
-                g=againGame;
-            }
-            if (g==1) {
-                return playLevel(9,3,0);
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Громко Игорь Дмитриевич", "Студент", "Mail1@mail.gb", "+77777777777", 5000, 22);
+        employees[1] = new Employee("Березин Иван Игоревич", "Программист", "Mail2@mail.gb", "+77777777777", 50000, 56);
+        employees[2] = new Employee("Языкова Алина Викторовна", "Администратор", "Mail3@mail.gb", "+77777777777", 100000, 38);
+        employees[3] = new Employee("Юлятова Рофл Владимировна", "Гл. бухгалтер", "Mail$$$@mail.gb", "+12345678910", 50000000, 68);
+        employees[4] = new Employee("Белов Александр Юрьевич", "Водитель", "Mail3@mail.gb", "+77777777777", 500, 35);
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getAge() > 40) {
+                employees[i].printInfo();
             }
         }
-        return (lives);
     }
 }
-
-
