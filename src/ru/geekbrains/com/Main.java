b968f5b7674cfb863a83ea2bea4f7857ee1c3017
IGrom7 <gold64rus@gmail.com>
2020-11-26 19:24:23 +0300

    Хочу сделать высчет ЗП(как калькулятор, но не хватает времени).

--- src/ru/geekbrains/com/Main.java
+++ src/ru/geekbrains/com/Main.java
@@ -4,24 +4,6 @@ package ru.geekbrains.com;
 
 public class Main {
     public static void main(String[] args) {
-        Plate plate = new Plate(15);
-        Cat[] cats = new Cat[3];
-        cats[0] = new Cat("Анис", 5);
-        cats[1] = new Cat("Фараон", 20);
-        cats[2] = new Cat("Рыжик", 30);
-
-
-        for (int i = 0; i < cats.length; i++) {
-            cats[i].eat(plate);
-        }
-        addFoodTo(plate, 10);
-        for (int i = 0; i < cats.length; i++) {
-            cats[i].eat(plate);
-        }
-       addFoodTo(plate, 30);
-            cats[2].eat(plate);
-    }
-    static void addFoodTo(Plate plate, int food) {
-        plate.addFood(food);
+        MyWindow w = new MyWindow();
     }
 }
